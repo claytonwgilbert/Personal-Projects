33ce5a5b9c7589569ddf9fc22f32ddb6a7e28696
Clayton Gilbert <claytonwgilbert@gmail.com>
2020-10-07 16:00:11 -0400

    Initial Commit

---
+++ pet-palace/src/main/java/com/cg/petpalace/PetPalaceApplication.java
@@ -0,0 +1,13 @@
+package com.cg.petpalace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetPalaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetPalaceApplication.class, args);
+	}
+
+}
---
+++ pet-palace/src/test/java/com/cg/petpalace/PetPalaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.petpalace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetPalaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
