2afa4b184d529335c306806394f79cb5d94269ec
Clayton Gilbert <claytonwgilbert@gmail.com>
2020-10-09 12:22:26 -0400

    Initial services implementation.

---
+++ pet-palace/pet-palace-data/src/main/java/com/cg/petpalace/services/PetService.java
@@ -0,0 +1,14 @@
+package com.cg.petpalace.services;
+
+import com.cg.petpalace.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
---
+++ pet-palace/pet-palace-data/src/main/java/com/cg/petpalace/services/VetService.java
@@ -0,0 +1,14 @@
+package com.cg.petpalace.services;
+
+import com.cg.petpalace.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
