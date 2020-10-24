9c65fbdf57bf96e1629a8ee72fd81e3031029d5a
Clayton Gilbert <claytonwgilbert@gmail.com>
2020-10-24 00:20:03 -0400

    Added PetTypeService interface and map implementation.

--- pet-palace/pet-palace-data/src/main/java/com/cg/petpalace/services/OwnerService.java
+++ pet-palace/pet-palace-data/src/main/java/com/cg/petpalace/services/OwnerService.java
@@ -3,5 +3,6 @@ package com.cg.petpalace.services;
 import com.cg.petpalace.model.Owner;
 
 public interface OwnerService extends CrudService<Owner, Long> {
+    //Custom method...
     Owner findByLastName(String lastName);
 }
---
+++ pet-palace/pet-palace-data/src/main/java/com/cg/petpalace/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.cg.petpalace.services;
+
+import com.cg.petpalace.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+}
