2b9a40eedbee356c51a7e0a2ce2d661178b1cb2a
Clayton Gilbert <claytonwgilbert@gmail.com>
2021-07-27 15:55:35 -0400

    Initial project creation brewery service

---
+++ mssc-service/mssc-service/src/main/java/com/cg/msscservice/MsscServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cg.msscservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscServiceApplication.class, args);
+	}
+
+}
---
+++ mssc-service/mssc-service/src/test/java/com/cg/msscservice/MsscServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.msscservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
