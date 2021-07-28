e1149835218286df51cc6ef6b2ecb797fa02f815
Clayton Gilbert <claytonwgilbert@gmail.com>
2021-07-28 19:10:35 -0400

    Added CRUD operations for beer in BrewerClient

--- mssc-brewery-client/src/test/java/com/cg/msscbreweryclient/web/client/BreweryClientTest.java
+++ mssc-brewery-client/src/test/java/com/cg/msscbreweryclient/web/client/BreweryClientTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.net.URI;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -18,6 +19,29 @@ public class BreweryClientTest {
     @Test
     public void getBeerById() {
         BeerDto beer = client.getBeerById(UUID.randomUUID());
+
         assertNotNull(beer);
     }
+
+    @Test
+    public void testSaveNewBeer() {
+        BeerDto beer = BeerDto.builder().beerName("Dark Lager").build();
+        URI uri = client.saveNewBeer(beer);
+
+        assertNotNull(uri);
+    }
+
+    @Test
+    public void testUpdateBeer() {
+        BeerDto beer = BeerDto.builder().beerName("Dark Lager").build();
+        client.updateBeer(UUID.randomUUID(), beer);
+    }
+
+    @Test
+    public void testDeleteBeer() {
+        client.deleteBeer(UUID.randomUUID());
+    }
+
+
+
 }
