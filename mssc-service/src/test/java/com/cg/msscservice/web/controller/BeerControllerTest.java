b000dc9d74d834a4037691526db5e09fd79c2fba
Clayton Gilbert <claytonwgilbert@gmail.com>
2021-07-27 20:09:17 -0400

    Adjusted pom file to include JUNIT 5 testing

---
+++ mssc-service/src/test/java/com/cg/msscservice/web/controller/BeerControllerTest.java
@@ -0,0 +1,28 @@
+package com.cg.msscservice.web.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest(BeerController.class)
+class BeerControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void getBeerById() {
+    }
+
+    @Test
+    void saveNewBeer() {
+    }
+
+    @Test
+    void updateBeer() {
+    }
+}
