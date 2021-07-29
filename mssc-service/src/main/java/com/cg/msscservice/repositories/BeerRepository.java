15c50dd43e4bbc9a394c67045dd59b3319de4e5b
Clayton Gilbert <claytonwgilbert@gmail.com>
2021-07-29 11:39:14 -0400

    Created data layaer as well as repository for Beer Service Microservice

---
+++ mssc-service/src/main/java/com/cg/msscservice/repositories/BeerRepository.java
@@ -0,0 +1,10 @@
+package com.cg.msscservice.repositories;
+
+
+import com.cg.msscservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
