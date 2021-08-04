c7e3c11db7cdac247a0a5f7458b3ae8b2e5d1a1c
Clayton Gilbert <claytonwgilbert@gmail.com>
2021-08-04 10:27:41 -0400

    Added sql scripts to beer service for data initialization

--- mssc-service/src/main/java/com/cg/msscservice/bootstrap/BeerLoader.java
+++ mssc-service/src/main/java/com/cg/msscservice/bootstrap/BeerLoader.java
@@ -3,13 +3,20 @@ package com.cg.msscservice.bootstrap;
 import com.cg.msscservice.domain.Beer;
 import com.cg.msscservice.repositories.BeerRepository;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
+import java.util.UUID;
 
+//@Component Not using this, using sql scripts to initialize data instead
 public class BeerLoader implements CommandLineRunner {
 
-    public static final String BEER_1_UPC = "00155001541002";
-    public static final String BEER_2_UPC = "00433001791012";
+    public static final String BEER_1_UPC = "0631234200036";
+    public static final String BEER_2_UPC = "0631234300019";
+    public static final String BEER_3_UPC = "0083783375213";
+    public static final UUID BEER_1_UUID = UUID.fromString("0a818933-087d-47f2-ad83-2f986ed087eb");
+    public static final UUID BEER_2_UUID = UUID.fromString("a712d914-61ea-4623-8bd0-32c0f6545bfd");
+    public static final UUID BEER_3_UUID = UUID.fromString("026cc3c8-3a0c-4083-a05b-e908048c1b08");
 
     private final BeerRepository beerRepo;
 
