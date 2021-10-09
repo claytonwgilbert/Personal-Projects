d26155968ec11ea47a81accf4637e9c7283f1f47
Clayton Gilbert <claytonwgilbert@gmail.com>
2021-10-09 04:59:37 -0400

    security updates

--- Angular/ecommerce-project/03-front-end/angular-commerce/src/app/config/my-app-config.ts
+++ Angular/ecommerce-project/03-front-end/angular-commerce/src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default {
     oidc: {
         clientId: '0oa242hpneoIzfWon5d7',
         issuer: 'https://dev-22845289.okta.com/oauth2/default', //communicated with to receive security tokens
-        redirectUri: 'http://localhost:4200/login/callback', //after login, redirect here
+        redirectUri: 'https://localhost:4200/login/callback', //after login, redirect here
         scopes: ['openid', 'profile', 'email']
 
     }
