b4a9f416f37278eee278258464a502c528dfc91b
Clayton Gilbert <claytonwgilbert@gmail.com>
2021-10-29 08:20:18 -0400

    Implemented error handling

--- Spring/spring-recipe-app/src/main/java/com/cg/springrecipeapp/controllers/RecipeController.java
+++ Spring/spring-recipe-app/src/main/java/com/cg/springrecipeapp/controllers/RecipeController.java
@@ -1,11 +1,16 @@
 package com.cg.springrecipeapp.controllers;
 
 import com.cg.springrecipeapp.commands.RecipeCommand;
+import com.cg.springrecipeapp.exceptions.NotFoundException;
 import com.cg.springrecipeapp.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
+@Slf4j
 @Controller
 public class RecipeController {
 
@@ -48,5 +53,20 @@ public class RecipeController {
         recipeService.deleteById(Long.valueOf(id));
 
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(Exception exception){
+
+        log.error("Handling not found exception");
+        log.error(exception.getMessage());
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("404error");
+        modelAndView.addObject("exception", exception);
+
+        return modelAndView;
     }
 }
