6a15745df6b2ca5968e54375d25e9b683aa80715
Clayton Gilbert <claytonwgilbert@gmail.com>
2021-11-10 16:52:16 -0500

    Initial Angular + Spring Boot nfl superbowl dashboard project

---
+++ Spring/nfl-superbowl-dashboard-backend/src/main/java/com/cg/nfl/superbowl/dashboard/backend/NflSuperbowlDashboardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cg.nfl.superbowl.dashboard.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NflSuperbowlDashboardBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NflSuperbowlDashboardBackendApplication.class, args);
+    }
+
+}
---
+++ Spring/nfl-superbowl-dashboard-backend/src/test/java/com/cg/nfl/superbowl/dashboard/backend/NflSuperbowlDashboardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.nfl.superbowl.dashboard.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NflSuperbowlDashboardBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
