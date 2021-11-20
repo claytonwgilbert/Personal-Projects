271e7384207eb9cd2d93f42fd525dc27262899c6
Clayton Gilbert <claytonwgilbert@gmail.com>
2021-11-20 04:17:02 -0500

    Initial project creation/setup

---
+++ Spring/shopme-commerce-project/shopme-project/src/main/java/com/cg/shopmeproject/ShopmeprojectApplication.java
@@ -0,0 +1,13 @@
+package com.cg.shopmeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopmeprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopmeprojectApplication.class, args);
+    }
+
+}
---
+++ Spring/shopme-commerce-project/shopme-project/src/test/java/com/cg/shopmeproject/ShopmeprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.shopmeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopmeprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
