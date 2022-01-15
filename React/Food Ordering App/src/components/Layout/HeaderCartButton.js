88d7254842ac6b8f6e44d5ed3d964cc166b662e9
Clayton Gilbert <claytonwgilbert@gmail.com>
2022-01-15 10:22:07 -0500

    Initial project creation Sloth

--- React/Food Ordering App/src/components/Layout/HeaderCartButton.js
+++ React/Food Ordering App/src/components/Layout/HeaderCartButton.js
@@ -11,13 +11,13 @@ function HeaderCartButton(props){
     }, 0);
 
     return (
-    <button className={styles.button} onClick={props.onClick}>
-        <span className={styles.icon}>
-            <CartIcon />
-        </span>
-        <span>Your Cart</span>
-        <span className={styles.badge}>{numberOfCartItems}</span>
-    </button>
+        <button className={styles.button} onClick={props.onClick}>
+            <span className={styles.icon}>
+                <CartIcon />
+            </span>
+            <span>Your Cart</span>
+            <span className={styles.badge}>{numberOfCartItems}</span>
+        </button>
     );
 };
 
